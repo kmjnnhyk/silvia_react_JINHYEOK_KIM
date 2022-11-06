0c23c0924696f8afb1a00da6773c6dbd898a0571
kmjnnhyk <rlawlsgur620@gmail.com>
2022-11-07 00:15:49 +0900

    🐛 fix: styled-components 적용 에러
    styled-components 공식 docs를 참고하여 pages/_document.tsx 파일 내에 로직 추가했습니다.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-
+import { NextPage } from 'next';
 import 'antd/dist/antd.css';
-
 import MyLayout from 'components/layout/MyLayout';
 
-const DashBoard: React.FC = () => (
+const DashBoard: NextPage = () => (
   <MyLayout>
     <div>
       <h1>여기는 index 페이지</h1>
